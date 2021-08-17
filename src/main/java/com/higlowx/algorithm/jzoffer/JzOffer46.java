4fa326e53792478cc3c9f53d51a4c43377d5dba6
李希浩 <lixihaofree@163.com>
2021-08-17 18:35:45 +0800

    commit

---
+++ src/main/java/com/higlowx/algorithm/jzoffer/JzOffer46.java
@@ -0,0 +1,27 @@
+package com.higlowx.algorithm.jzoffer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 圆圈中最后剩下的数字
+ * <p>
+ * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ * <p>
+ * leetcode：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * nowcoder：https://www.nowcoder.com/practice/f78a359491e64a50bce2d89cff857eb6
+ *
+ * @author Dylan.Li
+ * @date 2021/5/31
+ * @since
+ */
+
+public class JzOffer46 {
+
+    public int lastRemaining(int n, int m) {
+        return 0;
+    }
+
+
+}
